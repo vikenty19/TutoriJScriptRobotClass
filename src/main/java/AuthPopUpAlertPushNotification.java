fe797a4d4104f7abb5020f00c2f23989f4a76cab
vikenty19 <vikenty.plakhov@testpro.io>
2025-03-26 15:54:06 -0400

    Update AuthPopUpAlertPushNotification.java

--- src/main/java/AuthPopUpAlertPushNotification.java
+++ src/main/java/AuthPopUpAlertPushNotification.java
@@ -8,12 +8,13 @@ import java.time.Duration;
 
 public class AuthPopUpAlertPushNotification extends Demo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/basic_auth");
-        // authonticate user
+        // authenticate user you should enter your login and password admin is for demo only!
+        Thread.sleep(3000);
         driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");// admin --> login,admin-->password
 
         //Pop up notifications from Chrome
